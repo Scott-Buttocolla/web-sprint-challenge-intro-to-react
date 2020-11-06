25dbe317270f7339f9ae166a51ebfd69fb96d01b
scott-buttocolla@lambdastudents.com <sbuttocolla0810@gmail.com>
2020-11-06 09:22:26 -0800

    with MVP done, styling components now

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,22 +3,46 @@
 import React from "react";
 import styled from 'styled-components';
 
+const StyledCharacters = styled.div`
+/* display:flex; */
+    justify-items: center;
+    align-content:center;
+    justify-content:space-between;
+    border: 3px solid black;
+    width: 30%;
+    div{
+        display:flex;
+        justify-content:center;
+        border:1px solid green;
+    }
+
+
+
+
+
+
+`
+// back tic above
+
+
 
 
 // created Character per readMe and defined props as map and passing it the dataform App.js
 const Character = (props) => {
     const {data} = props
     return (
-        <div>
+        <StyledCharacters>
             <h2>{data.name}</h2>
+            <div>
             <p>Height: {data.height}</p>
             <p>Weight: {data.mass}</p>
             <p>Eye color: {data.eye_color}</p>
             <p>Skin color: {data.skin_color}</p>
             <p>Hair color: {data.hair_color}</p>
             <p>Gender: {data.gender}</p>
-            <p>Birth year: {data.birth_year}</p>  
-        </div>
+            <p>Birth year: {data.birth_year}</p>
+            </div> 
+        </StyledCharacters>
     )
 }
 
